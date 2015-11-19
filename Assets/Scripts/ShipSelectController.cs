2f59e72ef1092deddeece1b4d72b7a614716c4c2
EmanuelUmbelino <emanuel.umbelino@hotmail.com>
2015-11-19 10:23:14 -0200

    Version 0.15
    - Select Ship
    - Set Ship Position

---
+++ Assets/Scripts/ShipSelectController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipSelectController : MonoBehaviour {
+
+	public GameObject father;
+
+	private int select;
+
+	void OnMouseDown()
+	{
+		if(select.Equals(0))
+		{
+			this.GetComponent<SpriteRenderer> ().color = Color.blue;
+			select = 1;
+			TileLightsManager.shipSelect = this.gameObject;
+		}
+		else if (select.Equals(1)) 
+		{
+			this.GetComponent<SpriteRenderer> ().color = Color.green;
+			select = 2;
+			TileLightsManager.shipSelect = null;
+		}
+	}
+	public void SetPosition(Vector3 position)
+	{
+		if(select.Equals(1))
+			father.transform.position = position;
+	}
+}
--- Assets/Scripts/TileLightsManager.cs
+++ Assets/Scripts/TileLightsManager.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class TileLightsManager : MonoBehaviour {
 
+	public static GameObject shipSelect;
 
 	void Start () 
 	{
+		shipSelect = null;
 	}
 
 	void OnMouseEnter()
 	{
 		this.GetComponent<SpriteRenderer> ().color = Color.red;
+		try{shipSelect.GetComponent<ShipSelectController>().SetPosition(this.transform.position);}catch{}
+
+
 	}
 	void OnMouseExit()
 	{
