3fef83b084a5aac3740db6262c30d60d8ae63d6a
EmanuelUmbelino <emanuel.umbelino@hotmail.com>
2015-11-19 09:47:30 -0200

    Version 0.1
    - Tiles
    - Select Tile Atack

---
+++ Assets/Scripts/TileLightsManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileLightsManager : MonoBehaviour {
+
+
+	void Start () 
+	{
+	}
+
+	void OnMouseEnter()
+	{
+		this.GetComponent<SpriteRenderer> ().color = Color.red;
+	}
+	void OnMouseExit()
+	{
+		this.GetComponent<SpriteRenderer> ().color = Color.white;
+	}
+
+
+}
